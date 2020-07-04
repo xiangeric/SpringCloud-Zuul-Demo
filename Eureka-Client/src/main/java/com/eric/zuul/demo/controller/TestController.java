501a21a8478011eb129636080513fcc9ee893bbb
xiangeric <chenqx_gz@126.com>
2020-07-04 11:33:38 +0800

    add eureka client for zuul demo

---
+++ Eureka-Client/src/main/java/com/eric/zuul/demo/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.eric.zuul.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(HttpServletRequest request) {
+        System.out.println("----------------header----------------");
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            System.out.println(key + ": " + request.getHeader(key));
+        }
+        System.out.println("----------------header----------------");
+        return "hellooooooooooooooo!";
+    }
+}
